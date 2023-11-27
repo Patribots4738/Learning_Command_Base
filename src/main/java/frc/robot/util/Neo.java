e99d40f5cfeadf89daf7e6959f5e30bf352c5d4e
Jacob Hotz <77470805+Jacob1010-h@users.noreply.github.com>
2023-11-26 17:19:48 -0800

    Fix Neo motor initialization and add PID controller settings add setter methods for P, I, and D. Fix VELOCITY spelling. remove restore factory defaults

--- src/main/java/frc/robot/util/Neo.java
+++ src/main/java/frc/robot/util/Neo.java
@@ -6,6 +6,7 @@ import com.revrobotics.*;
 
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.Timer;
 import frc.robot.Robot;
 import frc.robot.util.Constants.FieldConstants;
 import frc.robot.util.Constants.NeoMotorConstants;
@@ -54,8 +55,8 @@ public class Neo extends CANSparkMax {
     public Neo(int id, boolean reversed, CANSparkMax.IdleMode mode) {
         super(id, CANSparkMaxLowLevel.MotorType.kBrushless);
 
-        // restoreFactoryDefaults();
-        // Timer.delay(0.050);
+        restoreFactoryDefaults();
+        Timer.delay(0.050);
 
         // If a parameter set fails, this will add more time to alleviate any bus traffic
         // default is 20ms
@@ -65,6 +66,7 @@ public class Neo extends CANSparkMax {
 
         encoder = getEncoder();
         pidController = getPIDController();
+        setIdleMode(CANSparkMax.IdleMode.kBrake);
     }
 
     /**
@@ -127,12 +129,12 @@ public class Neo extends CANSparkMax {
 
     private boolean shouldCache = false;
     private double position = 0;
-    private double velo = 0;
+    private double velocity = 0;
 
     public void tick() {
         if (shouldCache) {
             position = encoder.getPosition();
-            velo = encoder.getVelocity();
+            velocity = encoder.getVelocity();
         }
 
         if ((FieldConstants.IS_SIMULATION) && controlType == ControlLoopType.POSITION) {
@@ -171,7 +173,7 @@ public class Neo extends CANSparkMax {
      */
     public double getVelocity() {
         if (shouldCache) {
-            return velo;
+            return velocity;
         } else {
             return encoder.getVelocity();
         }
@@ -207,42 +209,108 @@ public class Neo extends CANSparkMax {
     }
 
     /**
-     * Gets the proportional gain constant for PIDFF controller.
-     * @return The proportional gain constant for PIDFF controller.
+     * The function sets the proportional gain of a PID controller.
+     * 
+     * @param gain The proportional gain determines the strength of the controller's response to
+     * the error between the desired setpoint and the actual process variable.
      */
+    public void setP(double gain) {
+        pidController.setP(gain);
+    }
+    
+    /**
+     * @param pidSlot The slot number of the PID controller, as you can have multiple
+     */
+    public void setP(double gain, int pidSlot) {
+        pidController.setP(gain, pidSlot);
+    }
+
+    /**
+     * The function sets the value of the I (integral) gain for a specified PID slot in a PID
+     * controller.
+     * 
+     * @param gain the proportional gain of the PID controller
+     */
+    public void setI(double gain) {
+        pidController.setI(gain);
+    }
+
+    /**
+     * @param pidSlot The slot number of the PID controller, as you can have multiple
+     */
+    public void setI(double gain, int pidSlot) {
+        pidController.setI(gain, pidSlot);
+    }
+
+    /**
+     * The function sets the value of the derivative gain for a PID controller.
+     * 
+     * @param gain The derivative gain determines the rate at which the controller
+     * responds to changes in the error signal. Think of them as "brakes"
+     */
+    public void setD(double gain) {
+        pidController.setD(gain);
+    }
+
+    /**
+     * @param pidSlot The slot number of the PID controller, as you can have multiple
+     */
+    public void setD(double gain, int pidSlot) {
+        pidController.setD(gain, pidSlot);
+    }
+
+    public void setPID(double p, double i, double d) {
+        setP(p);
+        setI(i);
+        setD(d);
+    }
+
+    public void setPID(double p, double i, double d, int pidSlot) {
+        setP(p, pidSlot);
+        setI(i, pidSlot);
+        setD(d, pidSlot);
+    }
+
+    public void setIZ(double gain) {
+        pidController.setIZone(gain);
+    }
+
+    public void setIZ(double gain, int pidSlot) {
+        pidController.setIZone(gain, pidSlot);
+    }
+
+    public void setFF(double gain) {
+        pidController.setFF(gain);
+    }
+
+    public void setFF(double gain, int pidSlot) {
+        pidController.setFF(gain, pidSlot);
+    }
+
+    public void setOutputRange(double min, double max) {
+        pidController.setOutputRange(min, max);
+    }
+
+    public void setOutputRange(double min, double max, int pidSlot) {
+        pidController.setOutputRange(min, max, pidSlot);
+    }
+
     public double getP() {
         return pidController.getP();
     }
 
-    /**
-     * Gets the integral gain constant for PIDFF controller.
-     * @return The integral gain constant for PIDFF controller.
-     */
     public double getI() {
         return pidController.getI();
     }
 
-    /**
-     * Gets the derivative gain constant for PIDFF controller.
-     * @return The derivative gain constant for PIDFF controller.
-     */
     public double getD() {
         return pidController.getD();
     }
 
-
-    /**
-     * Gets the I-Zone constant for PIDFF controller.
-     * @return The I-Zone constant for PIDFF control.
-     */
     public double getIZ() {
         return pidController.getIZone();
     }
 
-    /**
-     * Gets the feedforward gain constant for PIDFF controller.
-     * @return The feedforward gain constant for PIDFF controller.
-     */
     public double getFF() {
         return pidController.getFF();
     }
@@ -261,12 +329,12 @@ public class Neo extends CANSparkMax {
     // Documentation: https://docs.revrobotics.com/sparkmax/operating-modes/control-interfaces#periodic-status-frames
     public enum StatusFrame {
         APPLIED_FAULTS_FOLLOWER(PeriodicFrame.kStatus0, 10),
-        VELO_TEMP_VOLTAGE_CURRENT(PeriodicFrame.kStatus1, 20),
+        VELOCITY_TEMP_VOLTAGE_CURRENT(PeriodicFrame.kStatus1, 20),
         POSITION(PeriodicFrame.kStatus2, 20),
-        ANALOG_VOLTAGE_VELO_POS(PeriodicFrame.kStatus3, 50),
-        ALTERNATE_VELO_POS(PeriodicFrame.kStatus4, 20),
+        ANALOG_VOLTAGE_VELOCITY_POS(PeriodicFrame.kStatus3, 50),
+        ALTERNATE_VELOCITY_POS(PeriodicFrame.kStatus4, 20),
         ABSOLUTE_ENCODER_POS(PeriodicFrame.kStatus5, 200),
-        ABSOLUTE_ENCODER_VELO(PeriodicFrame.kStatus6, 200);
+        ABSOLUTE_ENCODER_VELOCITY(PeriodicFrame.kStatus6, 200);
 
         private final PeriodicFrame frame;
         private final int defaultPeriod; // ms
